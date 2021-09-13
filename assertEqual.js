a95ccaeff2f1e2e5db1382bdd5afe399493e8611
wei-paul <paul.wei607@gmail.com>
2021-09-13 23:49:28 +0000

    adding template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(" 😀 " + "Assertion Passed: " + actual + " === " + expected);
+    console.log(` 😀  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(" 😈 " + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(` 😈  Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
