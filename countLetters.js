57450cf8b49de6482afe80e9d3e785c88563221f
wei-paul <paul.wei607@gmail.com>
2021-09-16 01:23:04 +0000

    countLetters first commit

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` 😀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(` 😈  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let letterCount = {};
+  for (const letter of string) {
+    if (letter !== " ") {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters("Lighthouse"));
