809a74174543c0157551de92d0bc649efc3ccb7d
wei-paul <paul.wei607@gmail.com>
2021-09-20 21:10:53 +0000

    first implementation of mocha/chai on headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,15 @@
-const assertEqual = require('../assertEqual')
-const head = require('../head')
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  
+  });
+  
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  
+  });
+
+});
